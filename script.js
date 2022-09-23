0f1f7052ab4118b07ef9ff8b3d3e2955e54db6f9
Sreeram A <sriramlibra0@gmail.com>
2022-09-23 22:07:37 +0530

    home page ui

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 var searchResultContainer=document.getElementById('search-result-container');
-// hide-search-container
+
+const url="http://www.omdbapi.com/?t=wonderwomen&apikey=e16bcb3c";
+
+window.addEventListener('load',  async(e)=>{
+    // console.log("calling")
+   const res=await  fetch(url).then((data)=>JSON.parse(data));
+   console.log(res);
+})
